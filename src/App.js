17eb10b56581bd32cf8bba3ea2d08a6d9bd6db10
Jessica Anyanwu <jesax013@gmail.com>
2021-02-10 03:41:01 +0100

    Setup update
    - theming
    - prettier setup update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import { ThemeProvider } from "styled-components";
+import themes from "./common/utils/themes";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    var styleText =
+      "::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;";
+    var unfocus_style = document.createElement("STYLE");
+
+    window.unfocus = function () {
+      document.getElementsByTagName("HEAD")[0].appendChild(unfocus_style);
+
+      document.addEventListener("mousedown", function () {
+        unfocus_style.innerHTML = styleText;
+      });
+      document.addEventListener("keydown", function () {
+        unfocus_style.innerHTML = "";
+      });
+    };
+
+    window.unfocus();
+  });
+
   return (
-    <div className="App">
+    <ThemeProvider theme={themes} className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +40,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/common/utils/themes.js
@@ -0,0 +1,44 @@
+export default {
+  colors: {
+    primary: "#0675CE",
+    lgihtBlue: "#D5F2FB",
+    yellow: "#FCAB10",
+    white: "#FFFFFF",
+    bgGray: "#E5E5E5",
+    darkBgGray: "#C4C4C4",
+    gray90: "#4F4F4F",
+    gray80: "#646464",
+    gray30: "#E0E0E0",
+    gray20: "#F2F2F2",
+    black: "#000000",
+    black80: "#3B4146",
+    black70: "#454D53",
+    black60: "#58636D",
+    black40: "#95A1AC",
+  },
+  font: {
+    sizes: {
+      large: "20px",
+      normal: "14px",
+      small: "12px",
+    },
+    weight: {
+      light: 300,
+      normal: "normal",
+      bold: "500",
+      bolder: "bold",
+    },
+    line_height: {
+      large: "24px",
+      normal: "18px",
+      small: "16px",
+    },
+  },
+  border: {
+    radius: {
+      large: "32px",
+      medium: "9px",
+      small: "3px",
+    },
+  },
+};
